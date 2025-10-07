7e7ecd3383b6b8df10b1063a49cc75d7c6b99b3e
AngelF4 <angel.hdz.g@icloud.com>
2025-10-06 19:55:32 -0600

    Initial Commit

---
+++ AccesibleMap/AccesibleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccesibleMapApp.swift
+//  AccesibleMap
+//
+//  Created by Angel Hernández Gámez on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AccesibleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccesibleMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccesibleMap
+//
+//  Created by Angel Hernández Gámez on 06/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
